eb58f490777d5c7fb88dfda0f7135807bad32ae3
BEHİÇ OYTUN ŞENKUL <114110417+behicoytunsenkul@users.noreply.github.com>
2024-02-06 21:35:50 +0300

    Initial Commit

---
+++ myTrainingApp/myTrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTrainingAppApp.swift
+//  myTrainingApp
+//
+//  Created by Oytun Şenkul on 6.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct myTrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
